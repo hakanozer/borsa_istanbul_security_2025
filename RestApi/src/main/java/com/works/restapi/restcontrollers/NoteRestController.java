642f66d988f1d18ce93f8d09dc30fff4f440c53a
Hakan ÖZER <hakanozer02@gmail.com>
2025-08-04 14:08:08 +0300

    Update NoteRestController.java

--- RestApi/src/main/java/com/works/restapi/restcontrollers/NoteRestController.java
+++ RestApi/src/main/java/com/works/restapi/restcontrollers/NoteRestController.java
@@ -1,15 +1,29 @@
 package com.works.restapi.restcontrollers;
 
+import com.works.restapi.entities.Note;
+import com.works.restapi.entities.dtos.NoteAddDto;
 import com.works.restapi.repositories.NoteRepository;
+import com.works.restapi.services.NoteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("note")
 public class NoteRestController {
 
+    private final NoteService noteService;
 
+    @PostMapping("add")
+    public Note addNote(@RequestBody NoteAddDto noteAddDto) {
+        return noteService.addNote(noteAddDto);
+    }
+
+    @GetMapping("list")
+    public List<Note> getAllNote() {
+        return noteService.getAllNote();
+    }
 
 }
